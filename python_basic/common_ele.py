d56782cda5ff0dd6dedb299c225fd03c13995529
vinodnextcoder <vinodk2757@gmail.com>
2018-02-23 23:34:06 +0530

    list simple operation

---
+++ python_basic/common_ele.py
@@ -0,0 +1,6 @@
+List1=[1,2,3,4,5]
+List2=[2,5,3,6,7]
+for x in List1:
+    for y in List2:
+        if x==y:
+            print x,y
---
+++ python_basic/split_string.py
@@ -0,0 +1,6 @@
+string="vinod bhau khetade"
+list1=string.split(" ")
+print list1
+
+string='_'.join(list1)
+print string 
