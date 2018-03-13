70e20f2d607f553e09e6c2dd522e2c2c54664d47
vinodnextcoder <vinodk2757@gmail.com>
2018-03-13 22:57:36 +0530

    aded to cntrot

--- python_basic/countrot.py
+++ python_basic/countrot.py
@@ -1,7 +1,8 @@
  
-List = [15, 18, 2, 3, 6, 12]
+List = [45, 23, 12, 10, 22, 98]
 n = len(List)
 min = List[0]
+print "List Elemnts \n"
 print List
 for i in range(0, n):     
     if (min > List[i]):
