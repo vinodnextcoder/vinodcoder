92c44b91f6fa4cae971a608a78d8014e2cd606e4
vinodnextcoder <vinodk2757@gmail.com>
2018-04-11 23:33:49 +0530

    change in cnt code

--- python_basic/CountNumbers.py
+++ python_basic/CountNumbers.py
@@ -1,13 +1,19 @@
-List=[1,2,1,3,4,4,5]
-    count = 0
-    result = 0
-    string = input()
-    for i in string:
-        if i.isdigit():
-            count += 1
-        else:
-            if count >= 1:
-                result += 1
-                count = 0
+List=[1,2,1,1,3,5,2,3,5]
+List2=[]
+for k in set(List):
+    List2.append(int(k))
+print List
+print List2
+count = 0
+result = 0
 
-    print(result)
+for i in List:
+    
+    if i in List2:
+        count += 1
+    else:
+        if count >= 1:
+            result += 1
+            count = 0
+print count
+print(result)
