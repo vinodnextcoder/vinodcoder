e4be1803101d12d46962797dd571f9cd229f65cc
vinodnextcoder <vinodk2757@gmail.com>
2018-06-08 23:03:17 +0530

    char occr

---
+++ python_basic/countchar.py
@@ -0,0 +1,15 @@
+ACS=256
+def getcount(str1):
+    count=[0]*ACS
+    max=-1
+    c=-1
+    for i in str1:
+        count[ord(i)]+=1;
+    for i in str1:
+        if max < count[ord(i)]:
+            max=count[ord(i)]
+            c=i
+
+    return c
+str1="vinodiivinod"
+print getcount(str1)
