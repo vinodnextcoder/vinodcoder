3e2a3e711431a0f0312d4d5a31df04ccc0eb6ed6
vinodnextcoder <vinodk2757@gmail.com>
2018-07-03 20:58:48 +0530

    key handle exmple

--- python_basic/countchar.py
+++ python_basic/countchar.py
@@ -1,10 +1,12 @@
 ACS=256
 def getcount(str1):
     count=[0]*ACS
+    print count
     max=-1
     c=-1
     for i in str1:
         count[ord(i)]+=1;
+        print ord(i)
     for i in str1:
         if max < count[ord(i)]:
             max=count[ord(i)]
